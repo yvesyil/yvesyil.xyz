cf1fa6de3dbcdb69b19a94289e47eb69ec194e8d
yvesyil <doganyigityilmazz@gmail.com>
2025-09-15 23:55:43 +0200

    back to markdown syntax

--- .next/server/middleware-react-loadable-manifest.js
+++ .next/server/middleware-react-loadable-manifest.js
@@ -1,1 +1,1 @@
-self.__REACT_LOADABLE_MANIFEST="{}"
+self.__REACT_LOADABLE_MANIFEST="{\"app/components/PhotoCanvas.tsx -> ../lib/polaroid-cursor\":{\"id\":\"app/components/PhotoCanvas.tsx -> ../lib/polaroid-cursor\",\"files\":[\"static/chunks/_app-pages-browser_app_lib_polaroid-cursor_ts.js\"]}}"
---
+++ app/writings/1/layout.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import WritingLayout from '../../components/WritingLayout'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <WritingLayout>{children}</WritingLayout>
+}
+
+
